d4a34d8c017dcf9d081276ed328c04e8fc1d4c53
huyuhua33 <superjimalex@gmail.com>
2022-03-09 16:10:05 +0800

    Feature Point Added
    change target image

---
+++ map_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,81 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void EnableTracking::Awake()
+extern void EnableTracking_Awake_mC5E6FF77FFCE795BE52B4E9E78DD9B1F428C2BAE (void);
+// 0x00000002 System.Void EnableTracking::OnEnable()
+extern void EnableTracking_OnEnable_m8169AD6D7CB1373BFAD92BAC3DB9A058C69BF6DA (void);
+// 0x00000003 System.Void EnableTracking::OnDisable()
+extern void EnableTracking_OnDisable_m2C0B00E5DB6E667A433D5AB2960E5036DB8050E9 (void);
+// 0x00000004 System.Void EnableTracking::OnChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void EnableTracking_OnChanged_m34FF0A85CB84FB60D70FA7DB31B7FAF32E69C267 (void);
+// 0x00000005 System.Void EnableTracking::Start()
+extern void EnableTracking_Start_m5383C6189710B0A9462A0E40ADE054EE8674F6E4 (void);
+// 0x00000006 System.Void EnableTracking::Update()
+extern void EnableTracking_Update_mB641B53517E6831B4ECEFB41239B4B277A5C6F76 (void);
+// 0x00000007 System.Void EnableTracking::.ctor()
+extern void EnableTracking__ctor_m22A10F8AD7103EB755F1ADD80C55249EE3276D32 (void);
+// 0x00000008 System.Void ScrachPlat::Start()
+extern void ScrachPlat_Start_m6DE2FF6681D3462C7A0682778F7E88881DEC6ACF (void);
+// 0x00000009 System.Void ScrachPlat::Update()
+extern void ScrachPlat_Update_mDE10B6B451427251C2D1C040064ECB07C62B7469 (void);
+// 0x0000000A System.Void ScrachPlat::.ctor()
+extern void ScrachPlat__ctor_m328A44680CB529378038E051EAE68A30A11AB618 (void);
+static Il2CppMethodPointer s_methodPointers[10] = 
+{
+	EnableTracking_Awake_mC5E6FF77FFCE795BE52B4E9E78DD9B1F428C2BAE,
+	EnableTracking_OnEnable_m8169AD6D7CB1373BFAD92BAC3DB9A058C69BF6DA,
+	EnableTracking_OnDisable_m2C0B00E5DB6E667A433D5AB2960E5036DB8050E9,
+	EnableTracking_OnChanged_m34FF0A85CB84FB60D70FA7DB31B7FAF32E69C267,
+	EnableTracking_Start_m5383C6189710B0A9462A0E40ADE054EE8674F6E4,
+	EnableTracking_Update_mB641B53517E6831B4ECEFB41239B4B277A5C6F76,
+	EnableTracking__ctor_m22A10F8AD7103EB755F1ADD80C55249EE3276D32,
+	ScrachPlat_Start_m6DE2FF6681D3462C7A0682778F7E88881DEC6ACF,
+	ScrachPlat_Update_mDE10B6B451427251C2D1C040064ECB07C62B7469,
+	ScrachPlat__ctor_m328A44680CB529378038E051EAE68A30A11AB618,
+};
+static const int32_t s_InvokerIndices[10] = 
+{
+	2113,
+	2113,
+	2113,
+	1697,
+	2113,
+	2113,
+	2113,
+	2113,
+	2113,
+	2113,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	10,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
